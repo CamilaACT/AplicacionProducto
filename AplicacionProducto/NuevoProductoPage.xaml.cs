868388ad5140e70059cad83f6a3126b5da7e8811
CamilaACT <camilacabrera464@gmail.com>
2023-11-13 23:05:42 -0500

    Ya funciona navegar desde el inicio y agregar un producto y volver

--- AplicacionProducto/NuevoProductoPage.xaml.cs
+++ AplicacionProducto/NuevoProductoPage.xaml.cs
@@ -1,9 +1,29 @@
+using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+using System;
+
 namespace AplicacionProducto;
 
 public partial class NuevoProductoPage : ContentPage
 {
-	public NuevoProductoPage()
-	{
-		InitializeComponent();
-	}
+    public NuevoProductoPage()
+    {
+        InitializeComponent();
+
+
+    }
+    private async void OnclickGuardarProducto(object sender, EventArgs e)
+    {
+        int id = Utils.Utils.ListaProductos.Count+1;
+        Utils.Utils.ListaProductos.Add(new Models.Producto
+        {
+            IdProducto = id,
+            Nombre=Nombre.Text,
+            Descripcion=Descripcion.Text,
+            cantidad =Int32.Parse(cantidad.Text),
+        });
+        var tamaño = Utils.Utils.ListaProductos.Count;
+        Console.WriteLine($"Tamaño de la lista de productos");
+        await Navigation.PopModalAsync();
+    }
 }
--- AplicacionProducto/ProductoPage.xaml.cs
+++ AplicacionProducto/ProductoPage.xaml.cs
@@ -1,18 +1,39 @@
 namespace AplicacionProducto;
+
+using AplicacionProducto.Models;
 using CommunityToolkit.Maui.Alerts;
 using CommunityToolkit.Maui.Core;
-
+using System.Collections.ObjectModel;
 
 public partial class ProductoPage : ContentPage
 {
 	public ProductoPage()
 	{
 		InitializeComponent();
-		listaProductos.ItemsSource = Utils.Utils.ListaProductos;
-	}
-	private async void OnclickNuevoProducto(object sender, EventArgs e)
+        //listaProductos.ItemsSource = Utils.Utils.ListaProductos;
+        listaProductos.ItemsSource = new ObservableCollection<Producto>(Utils.Utils.ListaProductos);
+
+    }
+    private async void OnclickNuevoProducto(object sender, EventArgs e)
 	{
-		var toast = Toast.Make("On click Boton Nuevo Producto", ToastDuration.Short, 14);
-		await toast.Show();
-	}
+        try
+        {
+            await Navigation.PushModalAsync(new NuevoProductoPage());
+            var toast = Toast.Make("On click Boton Nuevo Producto", ToastDuration.Short, 14);
+            await toast.Show();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error al navegar a NuevoProductoPage: {ex.Message}");
+        }
+
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        //listaProductos.ItemsSource = Utils.Utils.ListaProductos;
+        listaProductos.ItemsSource = new ObservableCollection<Producto>(Utils.Utils.ListaProductos);
+    }
+
 }
